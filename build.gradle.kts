a9cdd7454330d8189c3ebdee2ebe5fec914c7443
azharkova <azharkova@usetech.ru>
2021-07-11 12:34:25 +0700

    version 1.50

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("multiplatform") version "1.4.32"
-    //id("maven-publish")
-    id("convention.publication")
+    kotlin("multiplatform") version "1.5.20"
+    id("maven-publish")
+    //id("convention.publication")
 }
 
 group = "io.github.anioutkazharkova"
-version = "1.0.4.5"
+version = "1.0.5.1"
 
 repositories {
     mavenCentral()
@@ -14,7 +14,7 @@ repositories {
 kotlin {
     jvm {
         compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
+            kotlinOptions.jvmTarget = "11"
         }
         testRuns["test"].executionTask.configure {
             useJUnit()
